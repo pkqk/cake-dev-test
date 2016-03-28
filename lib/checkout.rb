19472d69fabc3f3a15567663b52116df7ac6a067
Adam Sven Johnson <adam@pkqk.net>
2016-03-28 13:14:04 +0100

    Calculate basic totals without promotions

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,2 +1,29 @@
+class UnexpectedItemInTheBaggingArea < ArgumentError
+end
+
 class Checkout
+  PRODUCTS = {
+    "001" => 9.25,
+    "002" => 45.00,
+    "003" => 19.95
+  }
+
+  def initialize(promotional_rules)
+    @promotional_rules = promotional_rules || []
+    @items = []
+  end
+
+  def scan(sku)
+    raise UnexpectedItemInTheBaggingArea unless PRODUCTS[sku]
+    @items << sku
+    @promotional_rules.each { |rule| rule.add(sku) }
+  end
+
+  def total
+    gross = @items.inject(0.0) do |total, sku|
+      total + PRODUCTS[sku]
+    end
+    discounts = @promotional_rules.inject(0.0) { |discount, rule| discount + rule.discount }
+    gross - discounts
+  end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -6,10 +6,18 @@ describe Checkout do
     [%w[001 003 001], 36.95],
     [%w[001 002 001 003], 73.76]
   ]
+  promotional_rules = nil
 
   price_expectations.each do |skus, total|
     it "finds a total of £#{total} for products #{skus.join(', ')}" do
-      fail
+      co = Checkout.new(promotional_rules)
+      skus.each {|sku| co.scan(sku) }
+      expect(co.total).to eql(total)
     end
+  end
+
+  it "handles an unknown sku" do
+    co = Checkout.new(promotional_rules)
+    expect{ co.scan("missing") }.to raise_error(UnexpectedItemInTheBaggingArea)
   end
 end
